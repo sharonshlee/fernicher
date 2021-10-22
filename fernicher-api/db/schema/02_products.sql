5f7c7087d216ffcecb420590391394d028d58a20
Sharon Lee <sharonshlee@icloud.com>
2021-10-22 13:57:22 -0400

    edit db schema and seeds for products table

--- fernicher-api/db/schema/02_products.sql
+++ fernicher-api/db/schema/02_products.sql
@@ -4,5 +4,7 @@ CREATE TABLE products(
     id SERIAL PRIMARY KEY NOT NULL,
     product_name VARCHAR(255) NOT NULL,
     product_description TEXT,
+    image_url TEXT,
+    catgory_id INTEGER REFERENCES users(id)
     user_id INTEGER REFERENCES users(id)
 );
