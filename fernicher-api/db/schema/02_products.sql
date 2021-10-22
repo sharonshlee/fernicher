53ca7a298a51dedb74201a12251b1bb8c2edc8be
Lewis Lee <lewisforbusiness@gmail.com>
2021-10-22 11:54:43 -0400

    Add latitude and longitude properties to products schema/seeds

--- fernicher-api/db/schema/02_products.sql
+++ fernicher-api/db/schema/02_products.sql
@@ -4,5 +4,6 @@ CREATE TABLE products(
     id SERIAL PRIMARY KEY NOT NULL,
     product_name VARCHAR(255) NOT NULL,
     product_description TEXT,
-    user_id INTEGER REFERENCES users(id)
+    user_id INTEGER REFERENCES users(id),
+    product_location INTEGER ARRAY
 );
--- fernicher-api/db/seeds/02_products.sql
+++ fernicher-api/db/seeds/02_products.sql
@@ -1,53 +1,63 @@
 INSERT INTO
-products(product_name, product_description, user_id)
+products(product_name, product_description, user_id, product_location)
 VALUES
     (
         'Hey! Come back here! You big-a monkey!',
         'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        1
+        1,
+        ARRAY [43.865822, -79.619820]
     ),
     (
         'Nice of the princess to invite us over a picnic, eh Luigi?',
         'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        1
+        1,
+        ARRAY [43.889975, -79.560215]
     ),
     (
         'I wanna be a great plumber like my brother Mario.',
         'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        2
+        2,
+        ARRAY [43.689975, -79.390215]
     ),
     (
         'Luigi, Luigi! Oh yeah! Oh yeah!',
         'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        2
+        2,
+        ARRAY [43.189975, -79.890215]
     ),
     (
         'Did you see Toad and Chao and the sand?',
         'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        3
+        3,
+        ARRAY [42.889975, -77.590215]
     ),
     (
         'Aw, brutal!',
         'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        3
+        3,
+        ARRAY [43.829975, -79.590515]
     ),
     (
         'Whoo-hoo! Yippee!',
         'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        4
+        4,
+        ARRAY [43.889945, -79.580215]
     ),
     (
         'Aww, sweet! I did it, yay!',
         'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        4
+        4,
+        ARRAY [43.189975, -79.190215]
     ),
     (
         'Banana slamma!!',
         'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        5
+        5,
+        ARRAY [43.289975, -79.523215]
     ),
     (
         'Help, Diddy! I ve fallen, and I can t get up! Can you pass me a banana, little buddy?',
         'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        5
+        5,
+        ARRAY [43.119975, -79.230215]
     );
