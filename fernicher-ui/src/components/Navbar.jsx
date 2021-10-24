75f53b621594c69ad1b65ea80cdb5a951a301ae3
Sharon Lee <sharonshlee@icloud.com>
2021-10-23 20:28:03 -0400

    add body padding

--- fernicher-ui/src/components/Navbar.jsx
+++ fernicher-ui/src/components/Navbar.jsx
@@ -38,12 +38,12 @@ const useStyles = makeStyles((theme) => ({
     position: "relative",
     flexGrow: 1,
     borderRadius: 30,
-    backgroundColor: alpha(theme.palette.common.black, 0.05),
+    backgroundColor: alpha(theme.palette.common.black, 0.08),
     "&:hover": {
       backgroundColor: alpha(theme.palette.common.black, 0.15),
     },
     marginRight: theme.spacing(2),
-    marginLeft: 0,
+    marginLeft: 5,
     width: "100%",
     [theme.breakpoints.up("sm")]: {
       marginLeft: theme.spacing(3),
