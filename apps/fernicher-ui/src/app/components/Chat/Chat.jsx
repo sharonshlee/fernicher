a0d8c9f7854ffb24b5860f1c8885a5def1cb7ded
Lewis Lee <lewisforbusiness@gmail.com>
2021-10-27 16:33:46 -0400

    Create chat feature on homepage

--- apps/fernicher-ui/src/app/components/Chat/Chat.jsx
+++ apps/fernicher-ui/src/app/components/Chat/Chat.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ChatEngine } from 'react-chat-engine';
 import './Chat.css';
+import ChatFeed from './ChatFeed';
+import axios from 'axios';
+
+const user = {password: '123123', username: 'Lewis'};
 
 const Chat = () => {
+  const [username, setUsername] = useState(user.username);
+  const [password, setPassword] = useState(user.password);
+
   return (
     <ChatEngine
       height="100vh"
       projectID="9663adb6-d823-4a20-935f-b7381ba20105"
-      userName="Lewis"
-      userSecret="123123"
+      userName={username}
+      userSecret={password}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
     />
   )
 }
